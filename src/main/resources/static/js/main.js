1d84ac800294e7ffa83e4f5c6ee70e1a4446a398
Ranta <nehoya08@gmail.com>
2024-05-20 01:48:27 +0200

    configure ads

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,21 @@
-// 500 error
-$(function() {
-    setTimeout(function(){
-        $('body').removeClass('loading');
-    }, 1000);
-});
+var formdata = new FormData();
+formdata.append("type", "Vertical Strip");
+formdata.append("tags", "sunflowers");
+
+var ajax = new XMLHttpRequest();
+ajax.addEventListener("load", completeHandler, false);
+
+ajax.open("POST", "https://ad.simaneka.com/api/get");
+ajax.setRequestHeader("authorisation", "ZqplVXuSAv4tegoZ82IqWjFW6KS1lzfo");
+
+ajax.send(formdata);
+
+function completeHandler(event) {
+    var response = JSON.parse(event.target.responseText);
+
+    console.log(response);
+    document.querySelector('.advertIMG').src = response.link;
+    document.querySelector('.advertIMG').alt = response.alt;
+    document.querySelector('.anchorElement').href = response.href;
+    document.querySelector('.headerText').innerHTML = response.message;
+}
